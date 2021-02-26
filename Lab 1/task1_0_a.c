48c8bdd318addcc9923726ecd8ea97a396d0f3b7
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-02-25 22:05:42 -0500

    Update task1_0_a.c

--- Lab 1/task1_0_a.c
+++ Lab 1/task1_0_a.c
@@ -1,3 +1,29 @@
+/*
+0. (a) Extend code snippet 1 to check for read and write access permissions of a given file:
+
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <errno.h>
+ int main (int argc, char* argv[])
+ {
+   char* filepath = argv[1];
+   int returnval;
+   // Check file existence
+   returnval = access (filepath, F_OK);
+   if (returnval == 0)
+     printf ("\n %s exists\n", filepath);
+   else
+   {
+     if (errno == ENOENT)
+       printf ("%s does not exist\n", filepath);
+     else if (errno == EACCES)
+       printf ("%s is not accessible\n", filepath);
+return 0; }
+   // Check read access ...
+   // Check write access ...
+   return 0;
+}
+*/
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
