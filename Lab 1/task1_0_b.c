efb20f19c6f6b43a566583bec003c10a396074eb
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-02-25 22:03:40 -0500

    Create task1_0_b.c

---
+++ Lab 1/task1_0_b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main (int argc, char* argv[]) {
+    int fd;
+    //Opens file in read only mode and creates the file if it doesn't yet exist
+    fd = open(argv[1], O_RDONLY|O_CREAT);
+    //Checks for errors opening
+    if(fd == -1) {
+        perror("open");
+        return 1; 
+    }
+    else {
+        printf("\n Open() Successful\n");
+        /* open() succeeded, now one can do read operations on the
+        file opened since we opened it in read-only mode. Also once done
+        with processing, the file needs to be closed.*/
+    }
+    //Checks for errors closing
+    if (close(fd) != 0 ) {
+        perror("close");
+        return 1;
+    }
+    else {
+        printf("\n Close() Successful\n");
+    }
+    return 0;
+}
