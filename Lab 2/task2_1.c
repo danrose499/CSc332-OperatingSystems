09dd0fc05a38bf7d46de3e5ea1e26924a94a5e43
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-03-07 23:24:07 -0500

    Fixed wait (was in child instead of parent)

--- Lab 2/task2_1.c
+++ Lab 2/task2_1.c
@@ -15,12 +15,15 @@ int main() {
     int waitStatus[2];
     if(child1 == 0) { //Code for first child
         printf("\nI am first child, my pid is: %d\n", getpid());
-        wait(&waitStatus[0]);
     }
-    else { //Code for parent
+    else { //Code for parent after first child
+        wait(&waitStatus[0]); // Wait for first child to terminate
         int child2 = fork();
-        if(child2 == 0){ //Code for second child
+        if(child2 == 0){ // Code for second child
             printf("\nI am second child, my pid is: %d\n", getpid());
+            wait(&waitStatus[1]);
+        }
+        else { // Code for parent after second child
             wait(&waitStatus[1]);
         }
     }
