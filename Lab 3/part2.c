6f0e625c500fb11cb9e3a8fa1538dc9344ca867d
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-03-12 14:23:30 -0500

    Added task

---
+++ Lab 3/part2.c
@@ -0,0 +1,24 @@
+/*
+Task: Write a program where a child is created to execute a command that shows all files (including hidden files) in a directory with information such as permissions, owner, size, and when last modified. 
+Use execvp(...). Announce the successful forking of child process by displaying its PID.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {	
+	int pid_1 = fork();
+	if(pid_1 == 0) { // Child case
+		printf("Child PID: %d\n", getpid()); 
+        char *argv[] = {"ls", "-laxo", NULL};
+		execvp(argv[0],argv);  
+	}
+
+	else { // Parent case
+        wait(NULL);
+		printf("Parent PID: %d\n", getpid());   	
+	}
+	return 0;
+}
