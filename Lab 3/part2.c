974bb38ebd2c4e80d950df8b4120739e6ba2ca14
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-03-18 22:45:26 -0400

    got rid of extra header

--- Lab 3/part2.c
+++ Lab 3/part2.c
@@ -1,24 +1,25 @@
 /*
-Task: Write a program where a child is created to execute a command that shows all files (including hidden files) in a directory with information such as permissions, owner, size, and when last modified. 
+Task: Write a program where a child is created to execute a command that shows all files (including hidden files) 
+in a directory with information such as permissions, owner, size, and when last modified. 
 Use execvp(...). Announce the successful forking of child process by displaying its PID.
 */
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 int main() {	
-	int pid = fork();
+	int pid = fork();  
 	if(pid == 0) { // Child case
-		printf("Child PID: %d\n", getpid()); 
-        	char *argv[] = {"ls", "-laxo", NULL};
-		execvp(argv[0],argv);  
+		printf("Child PID: %d\n", getpid());  
+		char *argv[] = {"ls", "-laxo", NULL};
+		execvp(argv[0],argv);
+		printf("EXECVP Failed\n");	
 	}
 
 	else { // Parent case
-        	wait(NULL);
-		printf("Parent PID: %d\n", getpid());   	
+        wait(NULL);
+        printf("Parent PID: %d\n", getpid());		
 	}
 	return 0;
 }
